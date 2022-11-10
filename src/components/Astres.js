10b8419a00952d55d56ea9c00891c98dcf0e6138
E22C094T <mathurin.bourgouin@etu.univ-nantes.fr>
2022-11-10 13:38:20 +0100

    update radio & slide

--- src/components/Astres.js
+++ src/components/Astres.js
@@ -4,20 +4,59 @@ import Card from './Card';
 
 const Astres = () => {
 
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [selectedRadio, setSelectedRadio] = useState("");
+    const [rangeValue, setRangeValue] = useState(36);
 
     // Le useEffect se joue lorsque le composant est monté
     useEffect(() => {
         axios.get("https://api.le-systeme-solaire.net/rest/bodies/").then((res) => setData(res.data.bodies))
     }, [])
 
+    useEffect(() => {
+        let tempCat = [];
+        data.map((item) => (!tempCat.includes(item.bodyType) ? tempCat.push(item.bodyType) : null))
+        setCategories(tempCat);
+    }, [data])
+
     return (
         <div className='astres'>
-            <h1>Astres</h1>
+            <ul className='radio-container'>
+                <input
+                    type="range"
+                    min='1'
+                    max='250'
+                    defaultValue={rangeValue}
+                    onChange={(e) => setRangeValue(e.target.value)}
+                />
+                {categories
+                    .map((cat) => (
+                        <li>
+                            <input
+                                type="radio"
+                                id={cat}
+                                name="categroyRadio"
+                                checked={cat === selectedRadio}
+                                onChange={(e) => setSelectedRadio(e.target.id)}
+                            />
+                            <label htmlFor={cat}>{cat}</label>
+                        </li>
+                    ))}
+            </ul>
+            {selectedRadio && (
+                <button onClick={() => setSelectedRadio("")}>
+                    Annuler la sélection
+                </button>
+            )}
             <ul>
-                {data.map((astre, index) => (
-                    <Card key={index} astre={astre} />
-                ))}
+                {data
+                    .filter((astre) => astre.bodyType === selectedRadio)
+                    .sort((a, b) => a.name - b.name)
+                    .slice(0, rangeValue)
+                    .map((astre, index) => (
+                        <Card key={index} astre={astre} />
+                    ))}
             </ul>
         </div>
     );
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,7 @@ const Card = ({ astre }) => {
 
     return (
         <li className="card">
+            <img src="" alt={"planète " + astre.name} />
             <div className="infos">
                 <h2>Nom du pays</h2>
             </div>
